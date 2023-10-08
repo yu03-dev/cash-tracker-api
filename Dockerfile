73fd8e723868dee413fc695fca286de2d7ce80e5
yuto <yuto.0313king@icloud.com>
2023-10-08 12:27:18 +0900

    [add] 初回コミット

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM golang:1.19.0
+FROM golang:1.21.0
 
 WORKDIR /usr/src/app
 
-# RUN go install github.com/cosmtrek/air@latest
+RUN go install github.com/cosmtrek/air@latest
 
-# COPY . .
-# RUN go mod tidy
+COPY . .
+RUN go mod tidy
--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
-	fmt.Println("[Success]: Hello world!!")
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":3000"))
 }
